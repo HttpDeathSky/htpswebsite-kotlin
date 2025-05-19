1c370e80cf8b5de12e29bd13f7f1091d735aaeb3
HtpsDeathSky <1752405558@qq.com>
2025-05-19 14:36:59 +0800

    Init - Project Init~

---
+++ src/main/kotlin/htps/death/htpswebsitekotlin/HtpsWebSiteKotlinApplication.kt
@@ -0,0 +1,11 @@
+package htps.death.htpswebsitekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HtpsWebSiteKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<HtpsWebSiteKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/htps/death/htpswebsitekotlin/HtpsWebSiteKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package htps.death.htpswebsitekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HtpsWebSiteKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
